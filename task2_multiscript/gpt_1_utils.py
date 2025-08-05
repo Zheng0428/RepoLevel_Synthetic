c4255a59e697e0cdf54a54e26d517dff15bede23
tuney <tuney.zh@bytedance.com>
2025-08-05 23:24:51 +0800

    update buggy code gen

--- task2_multiscript/gpt_1_utils.py
+++ task2_multiscript/gpt_1_utils.py
@@ -15,7 +15,7 @@ from tqdm import tqdm
 from tqdm.asyncio import tqdm as async_tqdm  # 新增
 from temp_testbed import TempTestbed, get_all_filenames
 from utils import fake_git_repo
-from utils import get_llm_response as get_model_resposne #get_llm_response, get_deepseek_response, get_glm_response
+from utils import get_glm_response as get_model_resposne #get_llm_response, get_deepseek_response, get_glm_response
 from envs import DEFAULT_PATH, TRUE_PROJECT_FILE_LOC
 from utils import construct_three_shot_prompt_with_mutiscript as construct_prompt
 from utils import construct_unittest_prompt_with_mutiscript as construct_unittest_prompt
